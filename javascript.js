f40855a73aeb34b032c3690f3aa3dfcdb040927d
HelenaMadrid <helena.madrid@directrips.com>
2019-04-13 13:27:04 -0500

    para que no se borre

--- javascript.js
+++ javascript.js
@@ -29,17 +29,11 @@ $(document).ready(function () {
     $(".submit").on("click", function (event) {
         event.preventDefault();
         $(".containerSearch").attr("hidden", "true");
-
         var urlAPI = "https://api.songkick.com/api/3.0/search/artists.json?apikey=" + APIKEY + "&query=";
-
         //artist2 = $("#inputUserNav").val().trim();
         artist = $("#inputUser").val().trim();
-
         console.log("artist " + artist);
         console.log(artist2);
-
-        //https://api.songkick.com/api/3.0/artists/{artist_id}/calendar.json?apikey={your_api_key}
-
         if (artist2 === "") {
             artistfinal = artist;
 
@@ -49,7 +43,7 @@ $(document).ready(function () {
 
         }
 
-
+        //This first ajax call is searching in songkick the artist or band name. We need this to get the artistiD to do another ajax search for the event.
         $.ajax({
 
             url: urlAPI + artistfinal,
@@ -63,12 +57,14 @@ $(document).ready(function () {
             console.log(response.resultsPage.results.artist[0].displayName);
             console.log(artistID);
 
+            //this ajax call is using the artistID that we got from the previews ajax call. Here we get the tour calendar from the artist that the user input. This is still from the songkick API.
             $.ajax({
                 url: "https://api.songkick.com/api/3.0/artists/" + artistID + "/calendar.json?apikey=" + APIKEY,
                 method: "GET"
             }).then(function (responsetwo) {
                 console.log(responsetwo);
 
+                //Here we make a call using the ticketmaster API. First we search for the artist or band using the attractions with keyword. Here we find the artistID to do the search about the calendar and tour info in following ajax calls. The artistID we need to ensure that there are no cover bands in the results.
                 $.ajax({
                     type: "GET",
                     url: "https://app.ticketmaster.com/discovery/v2/attractions.json?apikey=Hc1CDHMvFbPBf3t8PaoGyuzOfvyTLPts&keyword=" + artistfinal,
@@ -77,150 +73,120 @@ $(document).ready(function () {
                     var ticketmasterArtistId = response3._embedded.attractions[0].id;
                     console.log(ticketmasterArtistId);
 
+                    //Now that we have the artistId, we look in events for all the events that have the artistid, this is to ensure that no cover bands to not show in the search. So here we find the eventID. We are limiting the results for 10 events.
                     $.ajax({
                         type: "GET",
                         url: "https://app.ticketmaster.com/discovery/v2/events.json?apikey=Hc1CDHMvFbPBf3t8PaoGyuzOfvyTLPts&attractionId=" + ticketmasterArtistId,
-                    }).then(function (response7) {
-                        console.log(response7);
+                    }).then(function (responsefour) {
+                        console.log(responsefour);
+                        var shortcutresponsefour = responsefour._embedded.events;
+                        var counter = 0;
 
+                        for(var y=0; y<shortcutresponsefour.length; y++){
+                            eventId = shortcutresponsefour[y].id;
+                            console.log(eventId);
+                        }
+                        
 
-                        $.ajax({
-                            type: "GET",
-                            url: "https://app.ticketmaster.com/discovery/v2/events/" + ticketmasterArtistId + ".json?apikey=Hc1CDHMvFbPBf3t8PaoGyuzOfvyTLPts",
-
-                        }).then(function (response4) {
-                            console.log(response4);
-                            //$("#events").append("<li><a href=" + results[0].uri + ">" + results[0].displayName + "</a></li>");
-                            for (var x = 0; x < 10; x++) {
-                                var responseShort = responsetwo.resultsPage.results.event[x];
-                                var container = $("#events");
-                                var imageConcert = $("<img>");
-                                var eventName = $("<p>");
-                                var venueName = $("<p>");
-                                var location = $("<p>");
-                                var dateTime = $("<p>");
-
-                                container.addClass("border border-white");
-
-
-                                var eventNameExtracted = responseShort.displayName;
-                                var venueNameExtracted = responseShort.venue.displayName;
-                                var locationExtracted = responseShort.location.city + " " + responseShort.venue.metroArea.displayName;
-                                var dateTimeExtracted = responseShort.start.date + " " + " " + responseShort.start.time;
-
-                                //$("#events").append(container);
-                                container.append(imageConcert);
-                                container.append(eventName);
-                                container.append(venueName);
-                                container.append(location);
-                                container.append(dateTime);
-
-                                eventName.text(eventNameExtracted);
-                                venueName.text(venueNameExtracted);
-                                location.text(locationExtracted);
-                                dateTime.text(dateTimeExtracted);
-                            }
-                        });
                     });
-                });
             });
         });
-        $("#inputUser").val("");
+    });
+    $("#inputUser").val("");
+});
+
+$("#signUpLink").off();
+$("#signUpLink").on("click", function (event) {
+    event.preventDefault();
+    if ($(".form-signin").attr("hidden") === "hidden") {
+        $(".form-signup").removeAttr("hidden");
+        $(".search").attr("hidden", "true");
+        $(".containerSearch").attr("hidden", "true");
+    }
+    else {
+        $(".form-signin").attr("hidden", "true");
+        $(".search").attr("hidden", "true");
+        $(".form-signup").removeAttr("hidden");
+        $(".containerSearch").attr("hidden", "true");
+    }
+});
+
+$("#signInLink").off();
+$("#signInLink").on("click", function (event) {
+    event.preventDefault();
+    if ($(".form-signup").attr("hidden") === "hidden") {
+        $(".form-signin").removeAttr("hidden");
+        $(".search").attr("hidden", "true");
+        $(".containerSearch").attr("hidden", "true");
+    }
+    else {
+        $(".form-signup").attr("hidden", "true");
+        $(".search").attr("hidden", "true");
+        $(".form-signin").removeAttr("hidden");
+        $(".containerSearch").attr("hidden", "true");
+
+    }
+});
+$("#signOutLink").off();
+$("#signOutLink").on("click", function (event) {
+    event.preventDefault();
+    window.location.href = "landingpage.html";
+});
+
+$("#signUp").off();
+$("#signUp").on("click", function (event) {
+    event.preventDefault();
+    var name = $("#inputUserName").val().trim();
+    var email = $("#inputEmail").val().trim();
+    var password = $("#inputPassword").val().trim();
+
+    console.log(name + email + password);
+
+    database.ref("users").push({
+        Name: name,
+        Email: email,
+        Password: password
     });
 
-    $("#signUpLink").off();
-    $("#signUpLink").on("click", function (event) {
-        event.preventDefault();
-        if ($(".form-signin").attr("hidden") === "hidden") {
-            $(".form-signup").removeAttr("hidden");
-            $(".search").attr("hidden", "true");
-            $(".containerSearch").attr("hidden", "true");
+    $("#inputUserName").val("");
+    $("#inputEmail").val("");
+    $("#inputPassword").val("");
+});
+
+$("#signIn").off();
+$("#signIn").on("click", function (event) {
+    event.preventDefault();
+    var emailInput = $("#emailSI").val().trim();
+    var passwordInput = $("#passwordSI").val().trim();
+
+    database.ref("users").on("child_added", function (childSnapshot) {
+        //event.preventDefault();
+        var emailDatabaseLocal = childSnapshot.val().Email;
+        var passwordDatabaseLocal = childSnapshot.val().Password;
+        //console.log("key "+keys);
+        console.log(emailDatabaseLocal + " " + passwordDatabaseLocal);
+
+        if (emailInput === emailDatabaseLocal && passwordInput === passwordDatabaseLocal) {
+            //emailDatabase = emailDatabaseLocal;
+            var keys = childSnapshot.key;
+            findUserKey = keys;
+            console.log("key " + keys);
+            $("#messageSignIn").text("This email is registered in our database");
+            console.log(emailInput + " is the same as " + emailDatabaseLocal);
+            console.log(passwordInput + " is the same as " + passwordDatabaseLocal);
+            window.location.href = "signedUser.html";
         }
         else {
-            $(".form-signin").attr("hidden", "true");
-            $(".search").attr("hidden", "true");
-            $(".form-signup").removeAttr("hidden");
-            $(".containerSearch").attr("hidden", "true");
+            $("#messageSignIn").html("<p>Sorry, the email or password is incorrect</p>");
+            console.log(emailInput + " is not the same as " + emailDatabaseLocal);
+            console.log(passwordInput + " is not the same as " + passwordDatabaseLocal);
         }
-    });
 
-    $("#signInLink").off();
-    $("#signInLink").on("click", function (event) {
-        event.preventDefault();
-        if ($(".form-signup").attr("hidden") === "hidden") {
-            $(".form-signin").removeAttr("hidden");
-            $(".search").attr("hidden", "true");
-            $(".containerSearch").attr("hidden", "true");
-        }
-        else {
-            $(".form-signup").attr("hidden", "true");
-            $(".search").attr("hidden", "true");
-            $(".form-signin").removeAttr("hidden");
-            $(".containerSearch").attr("hidden", "true");
-
-        }
-    });
-    $("#signOutLink").off();
-    $("#signOutLink").on("click", function (event) {
-        event.preventDefault();
-        window.location.href = "landingpage.html";
-    });
-
-    $("#signUp").off();
-    $("#signUp").on("click", function (event) {
-        event.preventDefault();
-        var name = $("#inputUserName").val().trim();
-        var email = $("#inputEmail").val().trim();
-        var password = $("#inputPassword").val().trim();
-
-        console.log(name + email + password);
-
-        database.ref("users").push({
-            Name: name,
-            Email: email,
-            Password: password
-        });
-
-        $("#inputUserName").val("");
-        $("#inputEmail").val("");
-        $("#inputPassword").val("");
-    });
-
-    $("#signIn").off();
-    $("#signIn").on("click", function (event) {
-        event.preventDefault();
-        var emailInput = $("#emailSI").val().trim();
-        var passwordInput = $("#passwordSI").val().trim();
-
-        database.ref("users").on("child_added", function (childSnapshot) {
-            //event.preventDefault();
-            var emailDatabaseLocal = childSnapshot.val().Email;
-            var passwordDatabaseLocal = childSnapshot.val().Password;
-            //console.log("key "+keys);
-            console.log(emailDatabaseLocal + " " + passwordDatabaseLocal);
-
-            if (emailInput === emailDatabaseLocal && passwordInput === passwordDatabaseLocal) {
-                //emailDatabase = emailDatabaseLocal;
-                var keys = childSnapshot.key;
-                findUserKey = keys;
-                console.log("key " + keys);
-                $("#messageSignIn").text("This email is registered in our database");
-                console.log(emailInput + " is the same as " + emailDatabaseLocal);
-                console.log(passwordInput + " is the same as " + passwordDatabaseLocal);
-                window.location.href = "signedUser.html";
-            }
-            else {
-                $("#messageSignIn").html("<p>Sorry, the email or password is incorrect</p>");
-                console.log(emailInput + " is not the same as " + emailDatabaseLocal);
-                console.log(passwordInput + " is not the same as " + passwordDatabaseLocal);
-            }
-
-            $("#emailSI").val("");
-            $("#passwordSI").val("");
+        $("#emailSI").val("");
+        $("#passwordSI").val("");
 
 
-        })
+    })
 
-    });
-
+});
 });
